59d5138999f8c024cf62b7699cdfa5b9ad635ee3
K1-1960 <k1.codingworld@gmail.com>
2023-09-04 17:52:28 -0700

    Inital commit

---
+++ src/index.js
@@ -0,0 +1,3 @@
+const http = require('./app.js');
+
+http.listen(3000, () => console.log('Servidor en el puerto 3000.'));
---
+++ src/routes/lilzoom.routes.js
@@ -0,0 +1,10 @@
+const { Router } = require('express');
+const path = require('path');
+
+const router = Router();
+
+router.get('/', (req, res) => {
+  res.redirect('index.html');
+});
+
+module.exports = router;
